ab7e316357080d3704136a9dbe499be1dcf31db7
Ethan Swafford <ethanswafford@yahoo.com>
2022-06-22 18:10:07 -0500

    devPush

---
+++ app.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelectorAll('.grid')
+    const width = 8
+
+    // create board
+    function createBoard() {
+        for (let i = 0; i < width * width; i++) {
+            const square = document.createElement('div')
+        }
+    }
+
+})
